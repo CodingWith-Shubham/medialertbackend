1d5dc46a14548fa33b6ff9d962b2fca40636f7b9
CodingWithShubham <mamgainshubham18@gmail.com>
2024-12-26 20:31:39 +0530

    Modified index.js to update cors

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ const express = require('express')
 var cors = require('cors')
 connectToMongo();
 const app = express()
+const corsOptions = {
+  origin: 'https://my-cloud-notes.netlify.app',  // Allow only requests from your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+  allowedHeaders: ['Content-Type', 'authtoken'], // Allow specific headers
+};
+
+app.use(cors(corsOptions)); // Apply CORS configuration to your app
 const port = process.env.PORT || 5000;
-app.use(cors())
 app.use(express.json())
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
   });
