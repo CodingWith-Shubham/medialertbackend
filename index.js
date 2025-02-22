8f37cb8bc5cb65c0e99200fd4fab0a2665932bf4
CodingWithShubham <mamgainshubham18@gmail.com>
2025-02-22 19:26:53 +0530

    modified index.js

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ var cors = require('cors')
 connectToMongo();
 const app = express()
 const corsOptions = {
-  origin: 'https://medialert.netlify.app/',  // Allow only requests from your frontend URL
+  origin: 'https://medialert.netlify.app',  // Allow only requests from your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
   allowedHeaders: ['Content-Type', 'authtoken'], // Allow specific headers
+  credentials: true,
 };
 
 app.use(cors(corsOptions)); // Apply CORS configuration to your app
+app.options('*', cors(corsOptions));
 const port = process.env.PORT || 5000;
 app.use(express.json())
 app.get('/', (req, res) => {
